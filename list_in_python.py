10b01501c8f0bd2e71afa83d85e83682988d684a
Harshit Malviya <harshitmalviya127@gmail.com>
2023-12-08 17:45:01 +0530

    Added List in Python

---
+++ list_in_python.py
@@ -0,0 +1,7 @@
+# list is implemented in python using array, list is also dynamic in nature
+# The only diiference btw list and array is list can store hetrogeneous elements while array store only homogeneous types of elements
+l1 = [12,23,"Harshit",23.12,True]
+print(l1)
+print(l1[3])
+l1.append(False)
+print(l1)
